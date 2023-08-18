46d674dcb556f6a0299dec919a7cffbefe7e3285
umairwho <umair.asad5@hotmail.com>
2023-08-18 11:34:11 +0500

    Added functionality to remove comments.

--- Preprocessor.java
+++ Preprocessor.java
@@ -28,7 +28,7 @@ class Preprocessor {
         }
 
         try {
-            formatAndSaveJavaFile(input_file);  // Call the method to format and save the output file
+            removeCommentsAndSaveJavaFile(input_file);  // Call the method to remove comments and save the output file
         } catch (IOException e) {
             System.out.println("Error writing output file: " + e.getMessage());
         }
@@ -36,36 +36,49 @@ class Preprocessor {
         System.out.println("here: " + input_file);
     }
 
-    // Method to format and save a Java file
-    private static void formatAndSaveJavaFile(String inputFileName) throws IOException {
+    // Method to remove comments and save a Java file
+    private static void removeCommentsAndSaveJavaFile(String inputFileName) throws IOException {
         String outputFileName = inputFileName + "_output.java"; // Create a new name for the output file
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
              BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
 
             String line;
-            int indentationLevel = 0;
+            boolean inBlockComment = false;
 
             while ((line = reader.readLine()) != null) {
                 String trimmedLine = line.trim();
+                
+                if (!inBlockComment) {
+                    int blockCommentStart = trimmedLine.indexOf("/*");
+                    int blockCommentEnd = trimmedLine.indexOf("*/");
+                    int lineComment = trimmedLine.indexOf("//");
+                    
+                    if (blockCommentStart >= 0 && blockCommentEnd > blockCommentStart) {
+                        inBlockComment = false;
+                        trimmedLine = trimmedLine.substring(0, blockCommentStart) + trimmedLine.substring(blockCommentEnd + 2);
+                    } else if (blockCommentStart >= 0 && blockCommentEnd < 0) {
+                        inBlockComment = true;
+                        trimmedLine = trimmedLine.substring(0, blockCommentStart);
+                    } else if (lineComment >= 0) {
+                        trimmedLine = trimmedLine.substring(0, lineComment);
+                    }
+                } else {
+                    int blockCommentEnd = trimmedLine.indexOf("*/");
+                    if (blockCommentEnd >= 0) {
+                        inBlockComment = false;
+                        trimmedLine = trimmedLine.substring(blockCommentEnd + 2);
+                    } else {
+                        trimmedLine = "";
+                    }
+                }
+                
                 if (!trimmedLine.isEmpty()) {
-                    // Adjust indentation
-                    if (trimmedLine.endsWith("}") || trimmedLine.endsWith(");")) {
-                        indentationLevel--;
-                    }
-
-                    for (int i = 0; i < indentationLevel; i++) {
-                        writer.write("\t"); // Use tabs for indentation
-                    }
-
-                    writer.write(line);
+                    writer.write(trimmedLine);
                     writer.newLine();
-
-                    if (trimmedLine.endsWith("{")) {
-                        indentationLevel++;
-                    }
                 }
             }
         }
     }
 }
+
